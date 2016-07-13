5ac191408f1a15aeef984998e18f1937b91646af
William Yang <yangwilliam92@gmail.com>
2016-07-13 14:35:05 -0400

    Table Add
    Added tables for restaurant and menuitem

--- database_setup.py
+++ database_setup.py
@@ -5,19 +5,35 @@ Beginning of File:
 Imports all modules needed & creates instance of declarative base
 """
 
+import os
 import sys
 
-from sqlalchemy import
-Column, ForeignKey, Integer, String
-
-from sqlalchemy.ext.declarative import
-declarative_base
-
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-
 from sqlalchemy import create_engine
 
 Base = declarative_base()
+
+class Restaurant(Base):
+__tablename__ = 'restaurant'
+name = Column(
+String(80), nullable = False)
+id = Column(
+Integer, primary_key = True)
+	)
+
+
+class MenuItem(Base):
+__tablename__ = 'menu_item'
+name = Column(String(80),nullable = False)
+id = Column(Integer, primary_key = True)
+course = Column(String(250))
+description = Column(String(250))
+price = Column(String(8))
+restaurant_id = Column(
+Integer, ForeignKey('restaurant.id'))
+restaurant = relationship(Restaurant)
 
 """
 End of File:
@@ -28,3 +44,4 @@ engine = create_engine(
 'sqlite:///restaurantmenu.db')
 
 Base.metadata.create_all(engine)
+
